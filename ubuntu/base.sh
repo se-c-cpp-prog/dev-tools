25a13ee39a6c0fdc3d8dd126970c4c36c550c639
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-09-14 01:41:36 +0300

    Setup building and archiving `GoogleTest`.

---
+++ ubuntu/base.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Update all packages links.
+sudo apt-get update
+
+# Install all basic utils.
+sudo apt-get install -y --no-install-recommends make cmake tar
