487d9d39650351d6f42d0d689f02665e4511afe9
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-09-14 09:59:41 +0300

    `ffmpeg` and `fftw`.

---
+++ ubuntu/ffmpeg.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+FFMPEG_VERSION='6.1'
+
+FFMPEG_URL='https://git.ffmpeg.org/ffmpeg.git'
+
+FFMPEG_SOURCES='ffmpeg-src'
+FFMPEG_BUILD='ffmpeg-build'
+FFMPEG_INSTALL='ffmpeg'
+
+FFMPEG_TAR="linux-${FFMPEG_INSTALL}-${FFMPEG_VERSION}.tar.gz"
+
+# Download FFmpeg sources.
+git clone "${FFMPEG_URL}" -b "release/${FFMPEG_VERSION}" "${FFMPEG_SOURCES}"
+
+# Set working directory to FFmpeg build.
+mkdir "${FFMPEG_BUILD}" && pushd "${FFMPEG_BUILD}"
+
+# Build and install FFmpeg.
+../"${FFMPEG_SOURCES}"/configure --disable-x86asm --enable-gpl --prefix="../${FFMPEG_INSTALL}/" --enable-shared --disable-static
+make -j"$(nproc --all)"
+make install
+
+# Go back.
+popd
+
+# Remove sources, build.
+rm -rf "${FFMPEG_SOURCES}" "${FFMPEG_BUILD}"
+
+# Compress installed.
+export GZIP=-9
+tar cvzf "${FFMPEG_TAR}" "${FFMPEG_INSTALL}"
+
+# Check compressed file.
+tar -tf "${FFMPEG_TAR}"
---
+++ ubuntu/fftw.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+FFTW_VERSION='3.3.10'
+
+FFTW_URL='https://www.fftw.org'
+
+FFTW_SOURCES="fftw-${FFTW_VERSION}"
+FFTW_DOWNLOADED_ARCHIVE="${FFTW_SOURCES}.tar.gz"
+FFTW_BUILD='fftw-build'
+FFTW_INSTALL='fftw'
+
+FFTW_TAR="linux-${FFTW_INSTALL}-${FFTW_VERSION}.tar.gz"
+
+# Download and extract FFTW sources.
+wget "${FFTW_URL}/${FFTW_DOWNLOADED_ARCHIVE}"
+tar -xf "${FFTW_DOWNLOADED_ARCHIVE}"
+rm "${FFTW_DOWNLOADED_ARCHIVE}"
+
+# Set working directory to ZLIB build.
+mkdir "${FFTW_BUILD}" && pushd "${FFTW_BUILD}"
+
+# Build and install FFTW (Single precision).
+cmake ../"${FFTW_SOURCES}"/ -D CMAKE_INSTALL_PREFIX=../"${FFTW_INSTALL}"/ -D ENABLE_FLOAT=ON -D BUILD_SHARED_LIBS=ON
+make -j"$(nproc --all)"
+make install
+
+# Go back.
+popd
+
+# Remove build.
+rm -rf "${FFTW_BUILD}"
+
+# Set working directory to ZLIB build.
+mkdir "${FFTW_BUILD}" && pushd "${FFTW_BUILD}"
+
+# Build and install FFTW (Double precision).
+cmake ../"${FFTW_SOURCES}"/ -D CMAKE_INSTALL_PREFIX=../"${FFTW_INSTALL}"/ -D ENABLE_DOUBLE=ON -D BUILD_SHARED_LIBS=ON
+make -j"$(nproc --all)"
+make install
+
+# Go back.
+popd
+
+# Remove sources, build.
+rm -rf "${FFTW_SOURCES}" "${FFTW_BUILD}"
+
+# Compress installed.
+export GZIP=-9
+tar cvzf "${FFTW_TAR}" "${FFTW_INSTALL}"
+
+# Check compressed file.
+tar -tf "${FFTW_TAR}"
