9ea32e2bb654bd0d7ea51231046d155bbe3a9cd7
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-09-14 15:55:39 +0300

    Setup Windows for `googletest` and `zlib`.

---
+++ windows/googletest.ps1
@@ -0,0 +1,32 @@
+#!/usr/bin/env pwsh
+
+$googletestVersion = '1.15.2'
+
+$googletestUrl = 'https://github.com/google/googletest.git'
+
+$googletestSources = 'googletest-src'
+$googletestBuild = 'googletest-build'
+$googletestInstall = 'googletest'
+
+$googletestZip = "windows-${googletestInstall}-${googletestVersion}.zip"
+
+# Download GoogleTest sources.
+git clone "${googletestUrl}" -b "v${googletestVersion}" "${googletestSources}"
+
+# Set working directory to GoogleTest build.
+mkdir "${googletestBuild}"
+Push-Location "${googletestBuild}"
+
+# Build and install GoogleTest.
+cmake ../"${googletestSources}"/ -D CMAKE_INSTALL_PREFIX=../"${googletestInstall}"/ -D BUILD_SHARED_LIBS=ON
+cmake --build . --target install --config Release
+
+# Go back.
+Pop-Location
+
+# Remove sources, build.
+Remove-Item -Recurse -Force "${googletestSources}"
+Remove-Item -Recurse -Force "${googletestBuild}"
+
+# Compress installed.
+Compress-Archive -CompressionLevel Optimal -Path "$googletestInstall" -DestinationPath "$googletestZip"
---
+++ windows/zlib.ps1
@@ -0,0 +1,32 @@
+#!/usr/bin/env pwsh
+
+$zlibVersion = '1.3.1'
+
+$zlibUrl = 'https://github.com/madler/zlib.git'
+
+$zlibSources = 'zlib-src'
+$zlibBuild = 'zlib-build'
+$zlibInstall = 'zlib'
+
+$zlibZip = "windows-${zlibInstall}-${zlibVersion}.zip"
+
+# Download ZLIB sources.
+git clone "${zlibUrl}" -b "v${zlibVersion}" "${zlibSources}"
+
+# Set working directory to ZLIB sources.
+mkdir "${zlibBuild}"
+Push-Location "${zlibBuild}"
+
+# Build and install ZLIB.
+cmake ../"${zlibSources}"/ -D CMAKE_INSTALL_PREFIX=../"${zlibInstall}"/ -D BUILD_SHARED_LIBS=ON
+cmake --build . --target install --config Release
+
+# Go back.
+Pop-Location
+
+# Remove sources, build.
+Remove-Item -Recurse -Force "${zlibSources}"
+Remove-Item -Recurse -Force "${zlibInstall}"
+
+# Compress installed.
+Compress-Archive -CompressionLevel Optimal -Path "${zlibInstall}" -DestinationPath "${zlibZip}"
