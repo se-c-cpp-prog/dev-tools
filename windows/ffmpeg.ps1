f1a365f13a68222f2025cb0f70dfee57b4606660
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-09-15 17:46:09 +0300

    Set location instead of pushing, install `make` in `pacman`.

--- windows/ffmpeg.ps1
+++ windows/ffmpeg.ps1
@@ -21,7 +21,7 @@ mkdir "${ffmpegInstall}"
 
 # Set working directory to FFmpeg build.
 mkdir "${ffmpegBuild}"
-Push-Location "${ffmpegBuild}"
+Set-Location "${ffmpegBuild}" # Should be Push-Location, but MSYS2 at start grabs actual working directory.
 
 # Turn on MSYS2 terminal.
 $msys2 = New-Object System.Diagnostics.Process
@@ -35,15 +35,15 @@ $msys2.Start()
 $msys2In = $msys2.StandardInput
 
 # Build and install FFmpeg.
-$commandConfigure = "bash ../${ffmpegSources}/configure --prefix=../${ffmpegInstall}/ --toolchain=msvc --disable-x86asm --enable-shared --disable-static --enable-gpl"
-$commandBuild = "make -j""(nproc --all)"""
+$commandInstallMake = 'pacman -S --noconfirm make'
+$commandConfigure = "../${ffmpegSources}/configure --prefix=../${ffmpegInstall}/ --toolchain=msvc --disable-x86asm --enable-shared --disable-static --enable-gpl"
+$commandBuild = 'make -j"(nproc --all)"'
 $commandInstall = 'make install'
 $commandExit = 'exit'
 
-$msys2In.WriteLine('')
-$msys2In.WriteLine('ls -al .')
-$msys2In.WriteLine('ls -al ..')
+$msys2In.WriteLine('') # This looks very dirty, but it's only way to prevent encoding error...
 
+$msys2In.WriteLine("${commandInstallMake}")
 $msys2In.WriteLine("${commandConfigure}")
 $msys2In.WriteLine("${commandBuild}")
 $msys2In.WriteLine("${commandInstall}")
@@ -53,7 +53,7 @@ $msys2In.WriteLine("${commandExit}")
 $msys2.WaitForExit()
 
 # Go back.
-Pop-Location
+Set-Location .. # See above.
 
 # Remove sources, build.
 Remove-Item -Recurse -Force "${ffmpegSources}"
